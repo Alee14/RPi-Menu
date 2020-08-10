44997f15409b638ec490f3286dc07d5ddc62f5d8
Andrew Lee <alee14498@protonmail.com>
2020-08-10 16:47:27 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-
+#!/usr/bin/env python3
 import tkinter
 import os
 from tkinter import *
